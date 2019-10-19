a1f5b7dcdf8d5cce5369737dcd5e116b21657238
Kunal Sunil Varudkar <varudkar.kunal@gmail.com>
2019-10-19 16:04:52 +0530

    Update reverse.py

--- Shellcode/reverse.py
+++ Shellcode/reverse.py
@@ -9,4 +9,4 @@ print 'Length of String:'+str(len(input))
 stringlist = [input[i:i+4] for i in range (0, len(input), 4)]
 
 for item in stringlist[::-1]:
-	print item[::-1] + ': 0x' + str(item[::-1].encode('hex'))
+	print item[::-1] + ': push 0x' + str(item[::-1].encode('hex'))
